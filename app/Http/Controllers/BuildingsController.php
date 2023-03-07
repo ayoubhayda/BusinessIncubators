5c65270defa9bb4de964b63aaa38a3064b166bc8
AYOUB-HAYDA <ayoubhayda22@gmail.com>
2023-03-07 15:57:01 +0100

    update building complete

--- app/Http/Controllers/BuildingsController.php
+++ app/Http/Controllers/BuildingsController.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Http\Requests\BuildingRequest;
+
+use App\Http\Requests\BuildingStore;
+use App\Http\Requests\BuildingUpdate;
 use Illuminate\Support\Str;
 use App\Models\Building;
 use App\Models\City;
 use App\Models\User;
-use App\Models\Floor;
 use Auth;
 
 class BuildingsController extends Controller
@@ -14,71 +15,46 @@ class BuildingsController extends Controller
     public function index()
     {
         $admin = Auth::user();
-        $buildings = $admin->role == 1? Building::all():$admin->buildings;
-        return view('admin.buildings.index')
-        ->with('buildings', $buildings)
-        ->with('admin', $admin);
-        
+        $buildings = $admin->role == 1 ? Building::all() : $admin->buildings;
+        return view('admin.buildings.index', compact('buildings', 'admin'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        return view('admin.buildings.create')
-        ->with('cities', City::all())
-        ->with('users', User::where('role', 0)->get());
+        $cities = City::all();
+        $users = User::where('role', 0)->get();
+        return view('admin.buildings.create', compact('cities', 'users'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(BuildingRequest $request)
+    public function store(BuildingStore $request)
     {
-        $request->validated();
-        $slug = uniqid().Str::of($request->name)->slug('-').".".$request->logo->extension();
-        $building = Building::create($request->except(['logo','user_id']) + ['logo' => $slug]);
-        $building->users()->attach($request->user_id);
-        $request->logo->move(public_path('images'), $slug);
+        $validatedData = $request->validated();
+        $slug = uniqid() . Str::slug($validatedData['name'], '-') . '.' . $validatedData['logo']->extension();
+        $validatedData['logo']->move(public_path('images'), $slug);
+        $validatedData['logo'] = $slug;
+        $building = Building::create($validatedData);
+        $building->users()->attach($validatedData['user_id']);
         return redirect()->route('buildings.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show($building) 
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Building $building) 
+    public function edit(Building $building)
     {
         $admin = Auth::user();
-        return view('admin.buildings.edit')
-        ->with('building', $building)
-        ->with('cities', City::all())
-        ->with('users', User::where('role', 0)->get())
-        ->with('admin', $admin);
+        $selectedUserId = optional($building->user)->id;
+        $cities = City::all();
+        $users = User::where('role', 0)->get();
+        return view('admin.buildings.edit', compact('building', 'cities', 'users', 'admin', 'selectedUserId'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(BuildingRequest $request,Building $building)
+    public function update(BuildingUpdate $request, Building $building)
     {
-        $request->validated();
-        $slug = uniqid().Str::of($request->name)->slug('-').".".$request->logo->extension();
-        $request->logo->move(public_path('images'), $slug);
-        $building->update($request->except(['logo']) + ['logo' => $slug]);
+        $validatedData = $request->validated();
+        $slug = uniqid() . Str::slug($validatedData['name'], '-') . '.' . $validatedData['logo']->extension();
+        $validatedData['logo']->move(public_path('images'), $slug);
+        $building->update(array_merge($validatedData, ['logo' => $slug]));
         return redirect()->route('buildings.index');
     }
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(Building $building)
     {
         $building->delete();
--- app/Http/Controllers/FloorsController.php
+++ app/Http/Controllers/FloorsController.php
@@ -1,18 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Http\Requests\FloorStore;
 use App\Http\Requests\FloorUpdate;
 use App\Models\Building;
 use App\Models\Floor;
+use Illuminate\Http\Request;
 
 class FloorsController extends Controller
 {
     public function index(Building $building)
     {
         return view('admin.floors.index')
-        ->with('building', $building)
-        ->with('floors', $building->floors);
+            ->with('building', $building)
+            ->with('floors', $building->floors);
     }
 
     /**
@@ -20,52 +22,63 @@ class FloorsController extends Controller
      */
     public function create(Building $building)
     {
-        return view('admin.floors.index')
-        ->with('floors', $building->floors);
+        return view('admin.floors.create')
+            ->with('building', $building);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(FloorStore $request,Building $building)
+    public function store(FloorStore $request, Building $building)
     {
-        $request->validated();
-        Floor::create($request->all() + ['building_id'=> $building->id ]);
+        $validatedData = $request->validated();
+        $validatedData['building_id'] = $building->id;
+
+        Floor::create($validatedData);
+
         return redirect()->route('floors.index', $building);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Building $building,Floor $floor) 
+    public function show(Building $building, Floor $floor)
     {
-        //
+        return view('admin.floors.show')
+            ->with('building', $building)
+            ->with('floor', $floor);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Floor $floor,Building $building) 
+    public function edit(Building $building, Floor $floor)
     {
-        return view('admin.floors.index')
-        ->with('floors', $building->floors);
+        return view('admin.floors.edit')
+            ->with('building', $building)
+            ->with('floor', $floor);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(FloorUpdate $request,Building $building,Floor $floor)
+    public function update(FloorUpdate $request, Building $building, Floor $floor)
     {
-        $request->validated();
-        $floor->update($request->all() + ['building_id'=> $building->id ]);
+        $validatedData = $request->validated();
+        $validatedData['building_id'] = $building->id;
+
+        $floor->update($validatedData);
+
         return redirect()->route('floors.index', $building);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy( Building $building,Floor $floor)
+    public function destroy(Building $building, Floor $floor)
     {
         $floor->delete();
-        return redirect()->route('floors.index', ['building' => $building->id]); }
+
+        return redirect()->route('floors.index', $building);
+    }
 }
